a14802e6521f456b13e254c58cc7a391a17ea2a8
jeffersoncharlles <contato@jefferdeveloper.com>
2022-12-19 13:34:23 -0300

    chore:static first layout fix responsive

--- web/src/components/AcademicEducation/index.tsx
+++ web/src/components/AcademicEducation/index.tsx
@@ -9,7 +9,7 @@ export const AcademicEducation = ({ ...rest }) => {
 
     return(
       <section className={styles.container}  {...rest}>
-        <h2 className={styles.container_title} data-aos="fade-left">Academic Education</h2>
+        <h2 className={styles.container_title} data-aos="fade-up">Academic Education</h2>
         <div className={styles.container_About}>
           <picture className={styles.picture} data-aos="fade-up"  data-aos-delay="300">
             <PhotoSvg urlImage='/me2.png'  />
