d54b0c7a69039e06b700cd924b63275e8e147b48
jeffersoncharlles <contato@jefferdeveloper.com>
2022-12-23 03:29:10 -0300

    chore:implementation more seo

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -19,6 +19,21 @@ export default function Home() {
         <title>Jefferson Charlles</title>
         <meta name="description" content="Portfolio Jefferson Charlles" />
 
+
+        {/*<!--========== Open Graph / Facebook ===========-->*/}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://jefferdeveloper.com/" />
+        <meta property="og:title" content="Portfolio Jefferson Charlles" />
+        <meta property="og:description" content="Portfolio Jefferson Charlles." />
+        <meta property="og:image" content="svg/metatag-jeffer.png" />
+
+          {/*<!--=========== Twitter ===============-->*/}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://jefferdeveloper.com/" />
+        <meta property="twitter:title" content="Portfolio Jefferson Charlles." />
+        <meta property="twitter:description" content="Jefferson Charlles Portfolio mostrar tudo que ja foi feito por mim ate hoje e mostrando as melhores habilidades" />
+        <meta property="twitter:image" content="svg/metatag-jeffer.png" />
+
       </Head>
       <Header menu={menuHelpers} />
       <Image src={BackgroundHero} alt="" className='-z-20 absolute top-0 left-0 right-0 bottom-0 w-screen ' />
